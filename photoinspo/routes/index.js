4668c71f35fbf80abdd30619b57147cf67e579a1
kkthaw <kthaw@csu.fullerton.edu>
2024-04-19 16:44:57 -0700

    Commit 1

--- photoinspo/routes/index.js
+++ photoinspo/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts");
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/createuser', async function(req, res, next) {
+  let createduser = await userModel.create({
+    username: "john",
+    password: "john",
+    posts: [],
+    email: "john@male.com",
+    fullName: "John Doe",
+  });
+  res.send(createduser);
 });
 
 module.exports = router;
--- photoinspo/routes/users.js
+++ photoinspo/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/photoinspo");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [],
+  dp: {
+    type: String, // Assuming the profile picture is stored as a URL or file path
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = router;
+const User = mongoose.model('User', userSchema);
